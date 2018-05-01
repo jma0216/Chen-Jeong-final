a3542bd187c0a813db0792f146708b81266ad1c0
5fi26 <yc34493@uga.edu>
2018-04-30 20:13:58 -0400

    wrong stuff

--- sh.cpp
+++ sh.cpp
@@ -22,7 +22,6 @@ void env(char **e);
 void syserr(char * msg);
 void checkIO(char **args);
 int checkBackground(char **args);
-void setMyShellEnv();
 
 extern char **environ; //env variables
 extern int errno;      // system error number
@@ -110,13 +109,6 @@ int checkBackground(char **args){
   return dont_wait;
 }
 
-void setMyShellEnv(){
-  char home_path[1024];
-  getcwd(home_path, 1024);
-  strcat(home_path, "/myshell");
-  setenv("shell", home_path, 1);
-}
-
 void sigint_handler(int signum){
   //give warning that the signal has been disabled
   // wait for ENTER to be pressed before returning to the command line.
@@ -138,9 +130,6 @@ int main(int argc, char ** argv){
 
   signal(SIGINT, sigint_handler); // catches the CTRL C signal and calls an interrupt handler
 
-
-  setMyShellEnv(); // get the shell environment
-
   //check access first
   if(argc > 1) {
     freopen(argv[1], "r", stdin);
@@ -148,15 +137,15 @@ int main(int argc, char ** argv){
 
   while(!feof(stdin)){
     cout << "1730sh:" << path << "$ ";
-    // fputs(prompt, stdout);
+
     if(fgets(buf, MAX_BUFFER, stdin)){
       arg = args;
-      *arg++ = strtok(buf,SEPARATORS);
+      *arg++ = strtok(buf," \t\n");
 
-      while ((*arg++ = strtok(NULL,SEPARATORS)));
+      while ((*arg++ = strtok(NULL, " \t\n")));
 
       checkIO(args); //check i/o redirections
-      dont_wait = checkBackground(args); // check for background execution (that is &)
+            dont_wait = checkBackground(args); // check for background execution (that is &)
 
       if (args[0]) {
         // if there was an input redirection (<) 
@@ -174,56 +163,54 @@ int main(int argc, char ** argv){
 
 	if (!strcmp(args[0],"echo")) { 
           pid = getpid(); // get process id
-          switch(pid = fork()){
-	  case -1:
-	    syserr((char*) "fork error");
-	  case 0:
-	    setenv("parent", getenv("shell"), 1); //set parent
-
+	  switch(pid = fork()){
+	  case -1: syserr((char*) "fork error");
+	  case 0: setenv("parent", getenv("shell"), 1); //set parent
+	    
 	    //i/o redirection for output files
-	    if(output == 1)
-	      freopen(outputFile, "w", stdout);
-	    else if(append == 1)
-	      freopen(outputFile, "a+", stdout);
-
-	    execvp (args[0], args);  //execute in the child thread
-	    syserr((char*)"execvp err");
-	  default:                
+	  if(output == 1)
+	    freopen(outputFile, "w", stdout);
+	  else if(append == 1)
+	    freopen(outputFile, "a+", stdout);
+	  
+	  execvp (args[0], args);  //execute in the child thread
+	  syserr((char*)"execvp err");
+	  default:
 	    if (!dont_wait) //determine background execution wait (&)
 	      waitpid(pid, &status, WUNTRACED);
-          }
-          continue;
-        }
+	}
+      	continue;
+      }
 	
         if (!strcmp(args[0],"quit")) { 
           break; //break the loop so the program returns and ends
         }
 
 	else{
-          pid = getpid();   
-          switch (pid = fork ()) { 
+          pid = getpid();
+	  
+          switch(pid = fork ()){
 	  case -1:
 	    syserr((char*)"fork error");
-	  case 0:
+	
+	  case 0:(pid = fork())
 	    setenv("parent", getenv("shell"), 1); //set parent
-	    //i/o redirection
 	    if(output == 1)
 	      freopen(outputFile, "w", stdout);
 	    else if(append == 1)
 	      freopen(outputFile, "a+", stdout); 
-
+	    
 	    execvp (args[0], args); //execute in child thread
 	    syserr((char*)"exec");
-	  default:                
-	    if (!dont_wait) //determine background execution wait (&)
-	      waitpid(pid, &status, WUNTRACED);
-	  }
-          continue;
-        }
+	  }//else if
+	default:
+	  if (!dont_wait) //determine background execution wait (&)
+	    waitpid(pid, &status, WUNTRACED);
+	}
+	continue;
       }
-
-    }
-
+    }  
   }
   return 0;
 }
+
